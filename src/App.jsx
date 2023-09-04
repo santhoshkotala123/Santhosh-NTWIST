193c3260dadd6b42caae7b9591a6cce95ee7f991
Kotala Santhosh <143922100+santhoshkotala123@users.noreply.github.com>
2023-09-04 14:10:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Section from './component/Section'
+import Article from './component/Article'
+import Aside from './component/Aside'
+import Sec2 from './component/Sec2'
+import Aside2 from './component/Aside2'
+import Footer from './component/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <title>NTWIST</title> 
+    <link rel="shortcut icon" href="" type="image/x-icon"></link>
+
+    
+        <Section></Section>
+        <Article></Article>
+        <Aside></Aside>
+        <Sec2></Sec2>
+        <Aside2></Aside2>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
